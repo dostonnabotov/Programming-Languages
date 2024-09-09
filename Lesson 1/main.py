950936153e31ab1f864521496038bbd6d8cd5abb
Doston Nabotov <dostonnabotov@gmail.com>
2024-09-09 09:47:01 +0300

    Reviewb basic stuff

--- Lesson 1/main.py
+++ Lesson 1/main.py
@@ -1,6 +1,22 @@
 # num = int(input("Enter a number: "))
 # print(num)
 
-a = "3";
-a =  float(a)
-print(a, type(a))
+# a = "3";
+# a =  float(a)
+# print(a, type(a))
+
+# a = int(input("Enter your age: "))
+
+# if a < 18:
+#   print("underage")
+# elif a == 100:
+#   print("congrats!")
+# else:
+#   print("age")
+
+
+# print("123" + 2) # error
+# print("123" * 2) # 123123
+
+print("Enter your", "name!",  sep=" *** ", end="\t") # Enter your *** name!   Hello
+print("Hello")
